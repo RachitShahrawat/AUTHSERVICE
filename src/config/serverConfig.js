ddde5a6608644027d2130cb114c64c8000ad3587
RachitShahrawat <rachitshahrawat@gmail.com>
2025-01-15 02:13:59 +0530

    added jwt create and verify token

--- src/config/serverConfig.js
+++ src/config/serverConfig.js
@@ -5,5 +5,6 @@ dotenv.config();
 
 module.exports={
     PORT:process.env.PORT,
-    SALT:bcrypt.genSaltSync(10)
+    SALT:bcrypt.genSaltSync(10),
+    JWT:process.env.JWT
 }
--- src/services/user-service.js
+++ src/services/user-service.js
@@ -1,4 +1,6 @@
 const UserRepository=require('../repository/user-repository');
+const jwt=require('jsonwebtoken');
+const {JWT}=require('../config/serverConfig');
 
 
 class UserService {
@@ -13,6 +15,28 @@ class UserService {
         }
         catch(error){
             console.log("Something went wrong in the service layer");
+            throw error;
+        }
+    }
+
+    createToken(user){
+        try{
+       const result=jwt.sign(user,JWT,{expiresIn:'1h'});
+       return result;
+        }
+        catch(error){
+            console.log("Something went wrong in token creation");
+            throw error;
+        }
+    }
+
+    verifyToken(token){
+        try{
+        const response=jwt.verify(token,JWT);
+        return response;
+        }
+        catch(error){
+            console.log("Something went wrong in token validation");
             throw error;
         }
     }
